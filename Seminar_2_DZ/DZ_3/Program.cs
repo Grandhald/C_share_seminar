5d201de6074eb8e57cd3a0b0c2982f71baea8245
Grandhald <kel_tua@mail.ru>
2023-06-16 23:33:25 +0300

    S2 DZ2 Weekend check

---
+++ Seminar_2_DZ/DZ_3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите цифру обозначающие день недели: ");
+int NumDayWeek = Convert.ToInt32(Console.ReadLine());
+if (NumDayWeek / 6 == 1 || NumDayWeek / 7 == 1) {
+    Console.WriteLine("Это день является выходным");
+}
+else {
+    Console.WriteLine("Это день не является выходным");
+}
