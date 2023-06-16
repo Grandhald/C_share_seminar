b2097758a65137884d4cb8b00d9383f264d26139
Grandhald <kel_tua@mail.ru>
2023-06-16 22:39:03 +0300

    S2 DZ_2 print number third

---
+++ Seminar_2_DZ/DZ_2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+if (number.Length>2) {
+    Console.WriteLine(number[2]);
+}
+else {
+    Console.WriteLine("Третьей цифры нет");
+}
