8a3f54e5965c90a87cc58c5a1fd3720e7d3613a2
Grandhald <kel_tua@mail.ru>
2023-06-16 21:06:26 +0300

    DZ_1 finding more number

---
+++ Seminar_1_DZ/DZ_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+string inputFist = Console.ReadLine();
+int numberFirst = Convert.ToInt32(inputFist);
+Console.WriteLine("Введите второе число: ");
+string inputSecond = Console.ReadLine();
+int numberSecond = Convert.ToInt32(inputSecond);
+if (numberFirst>numberSecond) {
+    Console.WriteLine($"Число {numberFirst} большее");
+}
+if (numberSecond>numberFirst) {
+    Console.WriteLine($"Число {numberSecond} большее");
+}
+else {
+    Console.WriteLine("Оба числа равны");
+}
