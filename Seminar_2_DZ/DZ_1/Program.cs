4d93b1f17074a8922a20d4dc8a8400df62d5d179
Grandhald <kel_tua@mail.ru>
2023-06-16 22:32:16 +0300

    DZ_1 print second number

---
+++ Seminar_2_DZ/DZ_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+if (number.Length>1) {
+    Console.WriteLine(number[1]);
+}
+else {
+    Console.WriteLine("Второй цифры нет");
+}
