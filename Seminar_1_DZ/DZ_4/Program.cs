2ccce8b930a5b950b3c4d4a0340822520ac95f0b
Grandhald <kel_tua@mail.ru>
2023-06-16 22:04:12 +0300

    DZ_3 even numbers from one to number

---
+++ Seminar_1_DZ/DZ_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число больше 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>1) {
+    for (int i = 0; i<number-1; i+=2){
+        Console.Write($"{i+2} ");
+    }
+}
+else {
+    Console.WriteLine("Ошибка, неверно введенные данные");
+}
