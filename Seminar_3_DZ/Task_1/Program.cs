8aaea0a467569c34d4558bf9d2a77a2dcda15d9a
Grandhald <kel_tua@mail.ru>
2023-06-19 15:41:58 +0300

    Task_1 <Reverse>

--- Seminar_3_DZ/Task_1/Program.cs
+++ Seminar_3_DZ/Task_1/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿string Inputed (string message) 
+{   
+  Console.WriteLine(message);
+  string number = System.Console.ReadLine();
+    if (number.Length == 5) 
+    {
+        return number;
+    } 
+    else
+    {
+        System.Console.WriteLine("Вы ввели неверные данные. Попробуйте еще раз.");
+        return Inputed(message);
+    }
+}
+
+string number = Inputed("Введите пятизначное число:");
+
+char [] reverse(string number)
+{
+    char [] reverse = number.ToCharArray().Reverse().ToArray();
+    return reverse; 
+
+}
+Console.WriteLine(reverse(number));   
