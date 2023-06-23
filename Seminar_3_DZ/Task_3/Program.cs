b2ee9b1f36a38b00f2350dcb8ae9eac605188488
Grandhald <kel_tua@mail.ru>
2023-06-23 11:26:43 +0300

    Task_3 <Raising a number to a power>

--- Seminar_3_DZ/Task_3/Program.cs
+++ Seminar_3_DZ/Task_3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int RedInt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void ExponentPrint(int number, double pow)
+{
+    for (int i = 1; i < number+1; i++)
+    {
+        double exp = Math.Pow(i, pow);
+        Console.WriteLine(exp);
+    }
+}
+
+int number = RedInt("Введите число: ");
+ExponentPrint(number, 3);
