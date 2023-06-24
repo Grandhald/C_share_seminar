d880dd661f209b405df2096439041f27b8ef3b97
Grandhald <kel_tua@mail.ru>
2023-06-24 23:20:16 +0300

    S5/DZ Task_2 S um of array numbers at odd position

---
+++ Seminar_5_DZ/Task_1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int Inputed (string message) 
+{   
+  Console.WriteLine(message);
+  int number = Convert.ToInt32(System.Console.ReadLine());
+  return number;
+}
+
+int [] CreateRandomArray(int len, int min = 100, int max = 1000)
+{
+    int [] numbers = new int [len];
+    Random rdn = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = rdn.Next(min, max);
+    }   
+    return numbers;
+}
+
+int CountEvenNumbers (int [] array)
+{
+    int count = 0;
+    foreach (int item in array)
+    {
+        if (item % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string PrintArray (int [] array)
+{
+    string massifPrint = "[";
+    foreach (int item in array)
+    {
+        massifPrint += $"{item}, ";
+    }
+    return massifPrint.Substring(0, massifPrint.Length-2)+"]";
+}
+
+int Length = Inputed("Введите длину массива: ");
+int [] arrayRndNum = CreateRandomArray(Length);
+int countEvnNum = CountEvenNumbers(arrayRndNum);
+Console.WriteLine($"В массиве чисел {PrintArray(arrayRndNum)} четных чисел {countEvnNum}"); 
---
+++ Seminar_5_DZ/Task_2/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задача 2: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+int Inputed (string message) 
+{   
+  Console.WriteLine(message);
+  int number = Convert.ToInt32(System.Console.ReadLine());
+  return number;
+}
+
+int [] CreateRandomArray(int len,int min, int max)
+{
+    int [] numbers = new int [len];
+    Random rdn = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = rdn.Next(min, max);
+    }   
+    return numbers;
+}
+
+string PrintArray (int [] array)
+{
+    string massifPrint = "[";
+    foreach (int item in array)
+    {
+        massifPrint += $"{item}, ";
+    }
+    return massifPrint.Substring(0, massifPrint.Length-2)+"]";
+}
+
+int SumOddPosNum(int [] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int Length = Inputed("Введите количество элементов в массиве: ");
+int min = Inputed("Введите минимальный порог случайных значений: ");
+int max = Inputed("Введите максимальный порог случайных значений: ");
+int [] arrayNum = CreateRandomArray(Length, min, max);
+int sumOddPosNum = SumOddPosNum(arrayNum);
+Console.WriteLine($"Сумма элементов в массиве {PrintArray(arrayNum)} стоящих на нечетных позициях ровна {sumOddPosNum}");
