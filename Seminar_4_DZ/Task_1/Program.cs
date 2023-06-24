502d2084756c16b316b9ec03f2d1d867ef4e2a84
Grandhald <kel_tua@mail.ru>
2023-06-24 18:28:14 +0300

    S4/DZ Task_1 OneNumber in power OneTwo

---
+++ Seminar_4_DZ/Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+double Inputed (string message) 
+{   
+  Console.WriteLine(message);
+  double number = Convert.ToDouble(System.Console.ReadLine());
+  return number;
+}
+
+double Pow(double number, double power)
+{
+    double numPow = 1;
+    for (int i = 0; i < power; i++)
+    {
+        numPow *= number;
+    }
+    return numPow;
+}
+ 
+ double number = Inputed("Введите число: ");
+ double power = Inputed("Введите степень числа: ");
+ double numberPow = Pow(number, power);
+ Console.WriteLine($"Число {number} в степени {power} ровно {numberPow}");
+ 
+
