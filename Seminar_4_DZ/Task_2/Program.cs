fde04244213ba7636720b2106b7c965bc9e5eaea
Grandhald <kel_tua@mail.ru>
2023-06-24 19:10:44 +0300

    S4/DZ Task_2 <Sum digit number>

---
+++ Seminar_4_DZ/Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int Inputed (string message) 
+{   
+  Console.WriteLine(message);
+  int number = Convert.ToInt32(System.Console.ReadLine());
+  return number;
+}
+
+int SumDigitsNumbers(int number)
+{
+    int sum = 0;
+    while (number>0)
+    {
+      sum += number % 10;
+      number /=  10;
+    }
+    return sum;
+}
+
+int num = Inputed("Введите число: ");
+int sumDigit = SumDigitsNumbers(num);
+Console.WriteLine(sumDigit); 
