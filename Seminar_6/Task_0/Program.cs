7981aa9d8ed2d9a438cb5919be38b4bc9a70341b
Grandhald <kel_tua@mail.ru>
2023-06-27 21:55:03 +0300

    Seminar_6

---
+++ Seminar_6/Task_0/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+
+int Inputed (string message) 
+{   
+  Console.WriteLine(message);
+  int number = Convert.ToInt32(System.Console.ReadLine());
+  return number;
+}
+
+int a = Inputed("Введите длину стороны а");
+int b = Inputed("Введите длину стороны b");
+int c = Inputed("Введите длину стороны c");
+
+string Chek(int a, int b, int c)
+{
+    if (a+b > c && b+c > a && c+a > b)
+    {
+        return "Даный треугольник может существовать";
+    }
+    return "Данный треугольник не может существовать";
+}
+
+Console.WriteLine(Chek(a, b, c));
---
+++ Seminar_6/Task_2/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 3: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int Inputed(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(System.Console.ReadLine());
+    return number;
+}
+
+int[] Fibbonaci(int number)
+{
+    int[] array = new int[number];
+    if (number == 1)
+    {
+        return array;
+    }
+    array[1] = 1;
+    for (int i = 2; i < number; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    string massifPrint = "[";
+    foreach (int item in array)
+    {
+        massifPrint += $"{item}, ";
+    }
+    Console.WriteLine(massifPrint.Substring(0, massifPrint.Length - 2) + "]");
+}
+
+
+void Fib(int number, int one, int two)
+{
+    Console.WriteLine(one);
+    int three = one+two;
+    if (number > 1)
+    {
+        Fib(number-1, two, three);
+    }
+}
+
+int number = Inputed("Введите число: ");
+Fib(number, 0, 1);
