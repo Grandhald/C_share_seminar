08c3a5860ac7eb07f2e66bdb284dac4b558f9027
Grandhald <kel_tua@mail.ru>
2023-06-27 22:54:50 +0300

    S6/Dz Task_1 <Count Possib input Numbers>

---
+++ Seminar_6_DZ/Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Inputed(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(System.Console.ReadLine());
+    return number;
+}
+
+int CountPossitivNumbers(int numbers)
+{
+    int count = 0;
+
+    for (int i = 0; i < numbers; i++)
+    {
+        int number = Inputed("Введите число");
+        if (number > 0 && numbers > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int CntNumbers = Inputed("Введите количество вводимых числе: ");
+int CntNumPos = CountPossitivNumbers(CntNumbers);
+Console.WriteLine($"Количество позитивных чисел ---> {CntNumPos}");
