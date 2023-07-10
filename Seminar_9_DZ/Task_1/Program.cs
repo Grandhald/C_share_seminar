8bb8a523785108430727dd01f31aa6cde9ec6ed8
Grandhald <kel_tua@mail.ru>
2023-07-10 18:27:58 +0300

    S9/DZ Task_1 <Interval of even numbers (recursion)>

---
+++ Seminar_9_DZ/Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int Inputed(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(System.Console.ReadLine());
+    return number;
+}
+
+void IntervalOfEvenNumbers(int start, int finish)
+{
+    if (start % 2 != 0)
+    {
+        start++;
+    }
+    if (start>finish)
+    {
+        return;
+    }
+    Console.Write(start + "\t");
+    IntervalOfEvenNumbers(start+2, finish);
+}
+
+int initValue = Inputed("Введите начальное значение: ");
+int finalValue = Inputed("Введите конеченое значение: ");
+
+Console.WriteLine();
+
+IntervalOfEvenNumbers(initValue, finalValue);
