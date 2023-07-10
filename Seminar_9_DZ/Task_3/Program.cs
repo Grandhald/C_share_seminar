0484987cc4edcd484b1b78200cd10d67792fb9fd
Grandhald <kel_tua@mail.ru>
2023-07-10 20:00:45 +0300

    S9/DZ Task_3 <Function Akkerman>

--- Seminar_9_DZ/Task_3/Program.cs
+++ Seminar_9_DZ/Task_3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+double Inputed(string message)
+{
+    Console.WriteLine(message);
+    double number = Convert.ToDouble(System.Console.ReadLine());
+    return number;
+}
+double FunctionAkkerman(double m, double n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m != 0 && n == 0)
+    {
+        return FunctionAkkerman(m - 1, 1);
+    }
+    else
+    {
+        return FunctionAkkerman(m - 1, FunctionAkkerman(m, n - 1));
+    }
+}
+
+double m = Inputed("Введите значение m: ");
+double n = Inputed("Введите значение n: ");
+
+Console.WriteLine();
+
+Console.WriteLine(FunctionAkkerman(m, n));
