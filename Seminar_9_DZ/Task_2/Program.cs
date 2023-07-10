b7764f3e848ece8dcc847d704d7330e2bc640eb2
Grandhald <kel_tua@mail.ru>
2023-07-10 18:46:11 +0300

    S9/DZ Task_2 <Sum numbers in interval from one number to another>

--- Seminar_9_DZ/Task_2/Program.cs
+++ Seminar_9_DZ/Task_2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int Inputed(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(System.Console.ReadLine());
+    return number;
+}
+
+int SumNumbersInterval(int start, int finish)
+{
+    if (start > finish)
+    {
+        return 0;
+    }
+    return start + SumNumbersInterval(start + 1, finish);
+}
+
+int initValue = Inputed("Введите начальное значение: ");
+int finalValue = Inputed("Введите конеченое значение: ");
+
+Console.WriteLine();
+
+Console.WriteLine($"Сумма натуральных чисел от {initValue} до {finalValue} --> {SumNumbersInterval(initValue, finalValue)}");
